b26ff7248a685b2eb0df1f867bc3a77674998467
ahh01 <145448378+ahh01@users.noreply.github.com>
2024-05-29 21:21:55 +0200

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, TextInput, View, Button, Alert } from 'react-native';
 
 export default function App() {
+  const [guess, setGuess] = useState('');
+  const [message, setMessage] = useState('');
+  const [numberToGuess, setNumberToGuess] = useState(generateRandomNumber());
+
+  function generateRandomNumber() {
+    return Math.floor(Math.random() * 100) + 1;
+  }
+
+  const handleGuess = () => {
+    const guessedNumber = parseInt(guess);
+    if (isNaN(guessedNumber)) {
+      Alert.alert('Ange ett nummer mellan 1 och 100');
+      return;
+    }
+
+    if (guessedNumber < numberToGuess) {
+      setMessage('För lågt!');
+    } else if (guessedNumber > numberToGuess) {
+      setMessage('För högt!');
+    } else {
+      setMessage('Rätt Svar! Startar om spelet...');
+      setNumberToGuess(generateRandomNumber());
+    }
+
+    setGuess('');
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Gissa Numret!</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Gissa här"
+        keyboardType="number-pad"
+        value={guess}
+        onChangeText={text => setGuess(text)}
+      />
+      <Button title="Gissa" onPress={handleGuess} />
+      {message !== '' && <Text style={styles.message}>{message}</Text>}
     </View>
   );
 }
@@ -13,8 +48,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 20,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    paddingLeft: 8,
+    marginBottom: 20,
+    width: '80%',
+  },
+  message: {
+    marginTop: 20,
+    fontSize: 18,
   },
 });
